76ba0df633257dea5093089021bf5a696662566b
Fabiola <156017469+FabisDGufm@users.noreply.github.com>
2025-01-29 10:23:56 -0600

    Callstack

---
+++ estructurasdedatos/stacks/callstack/callstack.py
@@ -0,0 +1,11 @@
+''''
+Callstack and debugging.
+'''
+
+text = 'hola!'
+
+def make_upper(text: str) -> str:
+    text = text + '!!!'
+    return text.upper()
+
+print(len(make_upper(text)))
---
+++ estructurasdedatos/stacks/callstack/recursive_callstack.py
@@ -0,0 +1,14 @@
+'''
+Recursive callstack.
+'''
+
+def countdown(n: int):
+
+    print(n)
+
+    if n == 0:
+        return None
+    
+    countdown(n - 1)
+
+countdown(3)
