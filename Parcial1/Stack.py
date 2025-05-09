3e0fa582901a617788b2cb5054efd9ae45e5ec6a
FabisDGufm <156017469+FabisDGufm@users.noreply.github.com>
2025-05-08 20:03:57 -0600

    Documentos faltantes

---
+++ Parcial1/Stack.py
@@ -0,0 +1,46 @@
+'''
+Stack implementation Tortola.
+Cambios en search
+'''
+from memory_profiler import profile
+
+class Stack:
+    def __init__(self, size: int):
+        self.max = size
+        self.elements = [None] * size
+        self.top = -1
+
+    def __repr__(self):
+        return f'Current stack: {self.elements} | Top: {self.top}'
+    
+    def push(self, val: str) -> None:
+        if self.top == self.max - 1:
+            print('Stack overflow')
+            return None
+        
+        self.top += 1
+        self.elements[self.top] = val
+
+    @profile
+    def pop(self) -> any:
+        if self.top == -1:
+            print('Stack underflow')
+            return None
+
+        val = self.elements[self.top]
+        self.elements[self.top] = None 
+        self.top -= 1
+        return val
+    
+    def peek(self) -> any:
+        if self.top == -1:
+            print('Stack underflow')
+            return None
+
+        return self.elements[self.top]
+    
+    def search(self, busca: str) -> int:
+        for i in range(self.top, -1, -1):
+            if self.elements[i] == busca:
+                return i
+        return -1
